ba4f16ebf7bca0d9e59cb3f09769e86e4665f1fb
marianakvirchiladze <marianakvirchiladze88@gmail.com>
2025-05-13 00:22:01 +0400

    last commit

--- src/app/(main)/products/details/[id]/page.jsx
+++ src/app/(main)/products/details/[id]/page.jsx
@@ -1,18 +1,23 @@
 import AddToCart from "@/components/AddToCart/AddToCart";
 import styles from "./page.module.css";
-const page = async ({ params }) => {
-  const { id } = await params;
+
+const Page = async ({ params }) => {
+  const { id } = params;
   const res = await fetch(`https://fakestoreapi.com/products/${id}`);
   const product = await res.json();
 
   return (
     <div className={styles.container}>
-      <p>{id}</p>
       <h2>{product.title}</h2>
+      <img
+        src={product.image}
+        alt={product.title}
+        className={styles.productImage}
+      />
       <p>{product.description}</p>
-      <AddToCart product={product}/>
+      <AddToCart product={product} />
     </div>
   );
 };
 
-export default page;
+export default Page;
--- src/components/SignOut/SignOut.jsx
+++ src/components/SignOut/SignOut.jsx
@@ -1,15 +1,21 @@
 "use client";
 
-import styles from "./SignOut.module.css";
+import styles from "./SignOut.module.css"; 
 import { useRouter } from "next/navigation";
 
 const SignOut = () => {
   const router = useRouter();
+  
   const handleSignOut = () => {
     localStorage.removeItem("user");
     router.replace("/");
   };
-  return <button onClick={handleSignOut}>Sign Out</button>;
+
+  return (
+    <button className={styles.button} onClick={handleSignOut}>
+      Sign Out
+    </button>
+  );
 };
 
 export default SignOut;
